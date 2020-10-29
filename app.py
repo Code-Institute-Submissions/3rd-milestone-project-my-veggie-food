e4e409e1421d5c5e80b3a0e4e81c05b1736eddb9
Maria Jose Barrientos <cotebarrientos@gmail.com>
2020-10-29 00:06:26 +0000

    Added images for the carousel, additionally installed Google fonts.

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title='Home')
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
