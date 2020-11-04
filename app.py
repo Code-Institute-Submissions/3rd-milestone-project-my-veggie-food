6f661824899a2b5fa2d376093add5f4a6c6c7afe
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2020-11-04 19:56:12 +0000

    Added Database integration

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 import os
-from flask import Flask, render_template
+from flask import  Flask, flash, render_template, redirect, request, session, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
@@ -24,6 +33,12 @@ def login():
 @app.route('/contact')
 def contact():
     return render_template('contact.html', page_title='Contact')
+
+
+@app.route('/recipes')
+def get_recipes():
+    recipes = mongo.db.recipes.find()
+    return render_template("recipes.html", recipes=recipes)
 
 
 if __name__ == '__main__':
