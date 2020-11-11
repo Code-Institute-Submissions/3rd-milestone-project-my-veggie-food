bae73eac2116c60a57cbd0732dae5580978f9e1e
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2020-11-11 14:21:13 +0000

    Implemented EmailJS

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,26 @@ $('#delete').on('shown.bs.modal', function () {
     $('#delete').trigger('focus');
   });
   
+// To send mail from Contact Page
+
+function sendMail(contactForm){
+    emailjs.send("gmail", "contact_template", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "message": contactForm.message.value
+    })
+    .then (
+        function (response){
+            //Add message to modal
+            response = $(".message-sent-text").text("We'll be in touch with you as soon as possible.");
+            //Only opens modal on successful response
+            $("#messageSent").modal("toggle");
+            $("#btn-close-modal").click(function(){
+                location.reload();
+            });
+        },
+        function(error){
+            alert("failed", error); // To block from loading a new page
+        });
+    return false;
+}
