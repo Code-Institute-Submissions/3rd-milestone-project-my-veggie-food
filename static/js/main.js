c438383a81817485de0b5ff40569cff4a9fd9620
DESKTOP-GDRJ55O\Maria <cotebarrientos@gmail.com>
2020-11-19 03:25:02 +0000

    Added Scroll back to top button

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,3 @@
-// Delete Modal
-
-$('#delete').on('shown.bs.modal', function () {
-    $('#delete').trigger('focus');
-  });
-  
 // To send mail from Contact Page
 
 function sendMail(contactForm){
@@ -26,4 +20,27 @@ function sendMail(contactForm){
             alert("failed", error); // To block from loading a new page
         });
     return false;
+}
+
+// Scroll back to top button
+// Example from w3schools.com
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
